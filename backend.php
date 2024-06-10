bcf50ec7a2c68ef6c73a6630935c04f2ab8eb319
Saltshin <SaltinStillWaters@gmail.com>
2024-06-10 13:47:44 +0800

    added error for required

--- backend.php
+++ backend.php
@@ -1,15 +1,63 @@
 <?php
 function input($id, $type, $label='', $placeholder='', $required = false)
 {
+    addSession($id, $type, $required);
+
     if (!empty($label))
         echo "<label for='{$id}'> {$label} </label> <br>";
 
-    session_start();
-
-    echo "<input type='text' id='{$id}' name='{$id}' value='{$_SESSION['user']['content']}'placeholder='{$placeholder}'> <br>";
+    echo "<input type='text' id='{$id}' name='{$id}' value='{$_SESSION['user'][$id]['content']}' placeholder='{$placeholder}'> <br>";
 
     if($required)
-        echo "<span style='color: red;'> {$_SESSION['user']['error']} <br></span>";
+        echo "<span style='color: red;'> {$_SESSION['user'][$id]['error']} <br></span>";
 
     echo "<br>";
+}
+
+function addSession(string $id, Type $type, bool $required=false)
+{
+    if (isset($_SESSION['user'][$id]))
+        return;
+    
+    $_SESSION['user'][$id] = ['content' => '', 'error' => '', 'type' => $type, 'required' => $required];
+}
+
+function updateContents()
+{
+    foreach ($_POST as $id => $content)
+    {
+        if ($id == 'submit')
+            continue;
+
+        $_SESSION['user'][$id]['content'] = $content;
+    }
+}
+
+function updateErrors()
+{
+    foreach ($_SESSION['user'] as $id => $key)
+    {
+        if ($id == 'submit' || !$key['required'])
+            continue;
+        
+        //blank content
+        if (!$_SESSION['user'][$id]['content'])
+        {
+            $_SESSION['user'][$id]['error'] .= '*required';
+        }
+        else
+        {
+            $_SESSION['user'][$id]['error'] = str_replace('*required', '', $_SESSION['user'][$id]['error']);
+        }
+
+
+    }
+}
+
+enum Type
+{
+    case Name;
+    case NumberInt;
+    case NumberStr;
+    case Password;
 }
--- index.php
+++ index.php
@@ -1,45 +1,5 @@
 <?php 
-function input($id, $type, $label='', $placeholder='', $required = false)
-{
-    addSession($id, $type, $required);
-
-    if (!empty($label))
-        echo "<label for='{$id}'> {$label} </label> <br>";
-
-    echo "<input type='text' id='{$id}' name='{$id}' value='{$_SESSION['user'][$id]['content']}'> <br>";
-
-    if($required)
-        echo "<span style='color: red;'> {$_SESSION['user'][$id]['error']} <br></span>";
-
-    echo "<br>";
-}
-
-function addSession(string $id, Type $type, bool $required=false)
-{
-    if (isset($_SESSION['user'][$id]))
-        return;
-    
-    $_SESSION['user'][$id] = ['content' => '', 'error' => '', 'type' => $type, 'required' => $required];
-}
-
-function updateContents()
-{
-    foreach ($_POST as $id => $content)
-    {
-        if ($id == 'submit')
-            continue;
-
-        $_SESSION['user'][$id]['content'] = $content;
-    }
-}
-
-enum Type
-{
-    case Name;
-    case NumberInt;
-    case NumberStr;
-    case Password;
-}
+require_once('backend.php');
 ?>
 
 <!DOCTYPE html>
@@ -67,9 +27,10 @@ if (!isset($_SESSION['user']))
     );
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) 
+if (isset($_POST['submit'])) 
 {
     updateContents();
+    updateErrors();
     header("Location: " . $_SERVER['REQUEST_URI']);
     exit();
 }
